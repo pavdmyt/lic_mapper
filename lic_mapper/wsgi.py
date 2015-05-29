e1cbbbe4aa083ed37b8fd08fe328af94955d9b0f
pavdmyt <pavdmyt@users.noreply.github.com>
2015-05-29 11:35:32 +0300

    Initial commit.

---
+++ lic_mapper/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for lic_mapper project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lic_mapper.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
