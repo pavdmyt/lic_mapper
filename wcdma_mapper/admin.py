7f9ef1b2efb95dc9c7e15097917a4289977bcfd9
pavdmyt <pavdmyt@users.noreply.github.com>
2015-05-30 10:08:33 +0300

    Admin initial setup.

--- wcdma_mapper/admin.py
+++ wcdma_mapper/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from wcdma_mapper.models import RAitem, FeatureID, OSSitem
 
-# Register your models here.
+
+class RAitemAdmin(admin.ModelAdmin):
+    list_display = ('item_code', 'description')
+
+
+class FeatureIdAdmin(admin.ModelAdmin):
+    list_display = ('feature_id', 'ra_item')
+
+
+class OSSitemAdmin(admin.ModelAdmin):
+    # !!! TODO: add column to display RA item also.
+    # This requires a small revision of DB schema (look in models.py).
+    list_display = ('item_code', 'description', 'feature_id')
+
+
+admin.site.register(RAitem, RAitemAdmin)
+admin.site.register(FeatureID, FeatureIdAdmin)
+admin.site.register(OSSitem, OSSitemAdmin)
--- wcdma_mapper/models.py
+++ wcdma_mapper/models.py
@@ -8,6 +8,9 @@ class RAitem(models.Model):
     def __unicode__(self):
         return self.item_code
 
+    class Meta:
+        verbose_name_plural = 'RA sales items'
+
 
 class FeatureID(models.Model):
     ra_item = models.ForeignKey(RAitem)
@@ -16,11 +19,18 @@ class FeatureID(models.Model):
     def __unicode__(self):
         return self.feature_id
 
+    class Meta:
+        verbose_name_plural = 'RA features IDs'
+
 
 class OSSitem(models.Model):
+    # !!! TODO: ra_item = models.ForeignKey(RAitem.item_code)
     feature_id = models.ForeignKey(FeatureID)
     item_code = models.CharField(max_length=50)
     description = models.CharField(max_length=128, default='Description.')
 
     def __unicode__(self):
         return self.item_code
+
+    class Meta:
+        verbose_name_plural = 'OSS sales items'
