2776ab893fff8ab9274ee745a2d4fcd5a87b8d4d
pavdmyt <pavdmyt@users.noreply.github.com>
2015-06-02 23:17:58 +0300

    Map page, template, view and URL mapping added.

--- wcdma_mapper/urls.py
+++ wcdma_mapper/urls.py
@@ -3,4 +3,7 @@ from wcdma_mapper import views
 
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'))
+                       url(r'^$', views.index, name='index'),
+                       url(r'^map/(?P<ra_item_input>[\w\.]+)/$',
+                           views.map_page,
+                           name='map_page'),)
--- wcdma_mapper/views.py
+++ wcdma_mapper/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from wcdma_mapper.models import RAitem, FeatureID, OSSitem
 
 
 def index(request):
     return render(request, 'wcdma_mapper/index.html')
+
+
+def map_page(request, ra_item_input):
+    context = {}
+
+    try:
+        ra_item = RAitem.objects.get(item_code=ra_item_input)
+        context['ra_item'] = ra_item
+
+        # Retrieve associated feature IDs.
+        f_id = FeatureID.objects.filter(ra_item=ra_item)
+        context['f_id'] = f_id
+
+        # Retrieve associated OSS items.
+        oss_item = OSSitem.objects.filter(feature_id=f_id)
+        context['oss_item'] = oss_item
+    except RAitem.DoesNotExist:
+        pass
+
+    return render(request, 'wcdma_mapper/map.html', context)
